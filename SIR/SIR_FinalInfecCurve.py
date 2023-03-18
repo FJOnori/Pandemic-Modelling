b9cb6601f77141dc0a688f9aad99a3046a1ed9cf
FJOnori <s1960329@ed.ac.uk>
2023-03-18 17:39:00 +0000

    AS

--- SIR/SIR_FinalInfecCurve.py
+++ SIR/SIR_FinalInfecCurve.py
@@ -8,50 +8,64 @@ from random import random, randint
 
 class SIR_Model():
 
-    def __init__(self) -> None:
+    def progress_bar(self, current, total, bar_length=100):
+        fraction = current / total
+        arrow = int(fraction * bar_length - 1) * '-' + '>'
+        padding = int(bar_length - len(arrow)) * ' '
+        ending = '\n' if current == total else '\r'
+        print(f'Progress: [{arrow}{padding}] {round((fraction*100),5)}%', end=ending)
 
-        COVIDdf = (pd.read_csv('owid-covid-data-uk.csv')).replace(np.nan, 0)
+
+    def __init__(self, FinalTime=1000, dt=0.01, I_initial=0.00005, RecoveryRate=(1/10), GraphRounding=4, ContactFactor=0.98) -> None:
+
+        COVIDdf             = (pd.read_csv('owid-covid-data-uk.csv')).replace(np.nan, 0)
         
-        infectioncurve = np.array(list(np.array(COVIDdf['new_cases_smoothed']))[:1000])
-        kernel_size = 5
-        kernel = np.ones(kernel_size) / kernel_size
-        self.TrueInfectionCurve = np.convolve(infectioncurve, kernel, mode='same')
+        self.dt             = dt
+        self.FinalTime      = FinalTime
+        self.iterations     = int(self.FinalTime/self.dt)
 
-        self.S_initial = 1 - 0.0001
-        self.I_inital = 0.0001
-        self.population = self.S_initial + self.I_inital
-        self.RecoveryRate = 0.1
-        self.ContactRate = np.array(COVIDdf['reproduction_rate'])[:1000]*self.RecoveryRate
+        self.population     = 1
+        self.I_inital       = I_initial
+        self.S_initial      = self.population - I_initial
+        
+        self.RecoveryRate   = np.round(RecoveryRate,3)
+        self.ContactRate    = np.array(COVIDdf['reproduction_rate'])[:1000]*self.RecoveryRate*ContactFactor
+        self.GraphRounding  = GraphRounding
 
     def NumInt(self):
 
-        S ,I, R, NI = [self.S_initial], [self.I_inital], [0], [0]
+        S ,I, R, NI, T = [self.S_initial], [self.I_inital], [0], [0],[0]
+        beta = np.repeat(self.ContactRate, self.dt**(-1))
+        NIRT = 0
 
-        for n in np.arange(0,1000):
+        for n in np.arange(0, self.iterations):
 
-               dS = - self.ContactRate[n]*(I[n]/self.population)*S[n] 
-               dI = self.ContactRate[n]*(I[n]/self.population)*S[n] - self.RecoveryRate*I[n]
-               dR = self.RecoveryRate*I[n]
-
-               self.population += dI + dR + dS
+               dS = (-beta[n]*(I[n]/self.population)*S[n])*self.dt
+               dI = (beta[n]*(I[n]/self.population)*S[n] - self.RecoveryRate*I[n])*self.dt
+               dR = (self.RecoveryRate*I[n])*self.dt
 
                S.append(S[n] + dS)
                I.append(I[n] + dI)
                R.append(R[n] + dR)
-               NI.append(self.ContactRate[n]*(I[n]/self.population)*S[n])
+               T.append(self.dt*n)
+               
+               self.progress_bar(n, self.iterations)
+            
+               NIRT += (beta[n]*(I[n]/self.population)*S[n])*self.dt
+               if n%(self.dt**(-1)) == 0:
+                    NI.append(NIRT); NIRT = 0
 
-        return np.array(S), np.array(I), np.array(R), np.array(NI)
+        return np.array(S), np.array(I), np.array(R), np.array(NI), np.array(T)
     
     def Lineplot(self):
-        S,I,R,NI = self.NumInt()
-        #plt.plot(self.TrueInfectionCurve, c='#bfacac', label="Infected", ls=":")
-        plt.plot(NI, c='#b81111', label="Infected")
-        plt.title("SIR Numerical Integration")
-        plt.ylabel("Proportion of Population Infected")
+        S,I,R,NI,T = self.NumInt()
+        plt.plot(T,I*67000000, c='#b81111', label="Infected")
+        plt.title("SIR Model Infections $\gamma = "+ str(self.RecoveryRate) + "$")
+        plt.ylabel("Infected Population")
         plt.xlabel("Time (days)")
-        plt.xlim(0,1000)
-        plt.ylim(0,0.0002)
-        plt.savefig("SIR/SIRFinalInfectionCurve.png", dpi=227)
+        plt.xlim(0,self.FinalTime)
+        plt.grid(ls=":", c="grey", axis='y')
+        plt.savefig("SIR/SIRFinalInfectionCurve-" + str(self.RecoveryRate) + ".png", dpi=227)
 
 if __name__ == "__main__":
     SIR = SIR_Model()
--- SIRS/SIRS_FinalInfecCurve.py
+++ SIRS/SIRS_FinalInfecCurve.py
@@ -11,7 +11,7 @@ class SIR_Model():
     def __init__(self) -> None:
         
         self.FinalTime = 1000
-        self.dt = 1
+        self.dt = 0.01
         self.iterations = int(self.FinalTime/self.dt)
         COVIDdf = (pd.read_csv('owid-covid-data-uk.csv')).replace(np.nan, 0)
         
@@ -20,40 +20,48 @@ class SIR_Model():
         kernel = np.ones(kernel_size) / kernel_size
         self.TrueInfectionCurve = np.convolve(infectioncurve, kernel, mode='same')
 
-        self.S_initial = 1 - 0.0001
-        self.I_inital = 0.0001
+        self.S_initial = 1 - 0.00002
+        self.I_inital = 0.00002
         self.population = self.S_initial + self.I_inital
-        self.RecoveryRate = 0.1
+        self.RecoveryRate = 1/7
         self.ContactRate = np.array(COVIDdf['reproduction_rate'])[:1000]*self.RecoveryRate
         self.ImmunityLossRate = 1/365
+    
+    def progress_bar(self, current, total, bar_length=100):
+        fraction = current / total
+        arrow = int(fraction * bar_length - 1) * '-' + '>'
+        padding = int(bar_length - len(arrow)) * ' '
+        ending = '\n' if current == total else '\r'
+        print(f'Progress: [{arrow}{padding}] {round((fraction*100),5)}%', end=ending)
 
     def NumInt(self):
 
-        S ,I, R, = [self.S_initial], [self.I_inital], [0]
+        S ,I, R, T = [self.S_initial], [self.I_inital], [0], [0]
+        beta = np.repeat(self.ContactRate, self.dt**(-1))
 
         for n in np.arange(0,self.iterations):
 
-               dS = - self.ContactRate[n]*(I[n]/self.population)*S[n] + self.ImmunityLossRate*R[n]
-               dI = self.ContactRate[n]*(I[n]/self.population)*S[n] - self.RecoveryRate*I[n]
-               dR = self.RecoveryRate*I[n] - self.ImmunityLossRate*R[n]
+               dS = (-beta[n]*(I[n]/self.population)*S[n] + self.ImmunityLossRate*R[n])*self.dt
+               dI = (beta[n]*(I[n]/self.population)*S[n] - self.RecoveryRate*I[n])*self.dt
+               dR = (self.RecoveryRate*I[n] - self.ImmunityLossRate*R[n])*self.dt
 
-               self.population += dI + dR + dS
+               self.progress_bar(n, self.iterations)
 
                S.append(S[n] + dS)
                I.append(I[n] + dI)
                R.append(R[n] + dR)
+               T.append(self.dt*n)
 
-        return np.array(S), np.array(I), np.array(R)
+        return np.array(S), np.array(I), np.array(R), np.array(T)
     
     def Lineplot(self):
-        S,I,R = self.NumInt()
-        plt.plot(self.TrueInfectionCurve, c='#bfacac', label="Infected", ls=":")
-        plt.plot(I, c='#b81111', label="Infected")
+        S,I,R,T = self.NumInt()
+        plt.plot(T,I*67000000, c='#b81111', label="Infected")
         plt.title("SIRS Numerical Integration")
-        plt.ylabel("Population Proportion")
+        plt.ylabel("Infected Population")
         plt.xlabel("Time (days)")
-        plt.xlim(0,1000)
-        plt.ylim(0,0.003)
+        plt.xlim(0,self.FinalTime)
+        plt.ylim(0,180000)
         plt.savefig("SIRS/SIRSFinalInfectionCurve.png", dpi=227)
 
 if __name__ == "__main__":
